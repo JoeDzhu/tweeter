988e76bb02fea07fab6881872aeb4603edca0796
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-25 18:41:34 +0000

    checked the server file,nothing to fix as it is not part of the assignment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 "use strict";
-
+//server part;
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
